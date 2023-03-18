e9ae3e589e5a29cfe3217c5ed8b9e171d8ed640e
frhack <frapas@gmail.com>
2023-03-18 03:09:36 +0100

    debug

--- Dual.py
+++ Dual.py
@@ -22,26 +22,17 @@ class D():
         return this.trace_unary(this.ad.primitives.pow)
 
     def trace_binary(this,op,that): 
-        return trace_op_binary( this.ad, op.do_apply(this,that),op,this,that )
-        #return trace_op_binary( this.ad, op.do_apply(this,that), op.get_lin(z,this,that))
+        z = op.do_apply(this,that)
+        this.ad.trace(op.get_lin(z,this,that))
+        return z
 
     def trace_unary(this,op):
-        return trace_op_unary( this.ad, op.do_apply(this),op,this )
+        z = op.do_apply(this)
+        this.ad.trace(op.get_lin(z,this))
+        return z
 
     def __str__(self): return(f'D({self.p},{self.t})')
-
-def trace_op_binary(ad,z,op,this,that):
-    l = op.get_lin(z,this,that)
-    ad.trace(l)
-    return z 
-
-def trace_op_unary(ad,z,op,this):
-    l = op.get_lin(z,this)
-    ad.trace(l)
-    return z 
 
 def sin(this): return this.trace_unary(this.ad.primitives.sin)
 def cos(this): return this.trace_unary(this.ad.primitives.cos)
 def tanh(this): return this.trace_unary(this.ad.primitives.tanh)
-
-
